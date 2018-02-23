c921354defeb38404351be6cbde79bdec0f2ab6c
Casey J Law <caseyjlaw@gmail.com>
2018-02-23 08:37:57 -0800

    adding future as requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
 from setuptools import setup, find_packages
 
 setup(name='evla_mcast',
@@ -7,7 +12,7 @@ setup(name='evla_mcast',
       author='Paul Demorest',
       author_email='pdemores@nrao.edu',
       url='https://github.com/demorest/evla_mcast/',
-      install_requires=['lxml'],
+      install_requires=['lxml', 'future'],
       packages=find_packages(exclude=('tests',)),
       package_data={'evla_mcast': ['xsd/*.xsd','xsd/vci/*.xsd','xsd/observe/*.xsd']},
      )
